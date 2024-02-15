979e982c48b9ca392282b6ea8255a2d13cb1f0e9
Ludivine <ludivine.tulcibiez@gmail.com>
2024-02-15 10:40:52 +0100

    ajout exo2

--- exo2.py
+++ exo2.py
@@ -2,19 +2,47 @@
 # Partie 1 - Variables et Conditions simples :
 # Demandez à l'utilisateur de saisir son âge.
 # Stockez cette valeur dans une variable appelée age.
+
+age = 31
 # Utilisez une condition pour vérifier si l'âge est supérieur ou égal à 18.
+
+if age >= 18:
+    print("vous etes majeur")
 # Affichez "Vous êtes majeur !" si la condition est vraie, sinon affichez "Vous êtes mineur."
+
 # Partie 2 - Types de Variables :
 # Créez une nouvelle variable appelée nom et demandez à l'utilisateur de saisir son nom.
+
+nom = "lulu"
+taille = 1.65
 # Créez une troisième variable appelée taille et demandez à l'utilisateur de saisir sa taille (en mètres).
 # Utilisez des conditions pour déterminer si la taille de la personne est supérieure à 1.75 mètres. Affichez un message approprié en conséquence.
+
+if taille >= 1.75:
+    print ("ts ok")
+else:
+    print ("t'es un naim")
+
 # Partie 3 - Manipulation de Chaînes de Caractères :
 # Concaténez le nom et l'âge pour créer une nouvelle variable appelée message.
 # Affichez le message résultant.
+
+message = nom + str(taille)
+
+print (message)
 # Partie 4 - Types de Variables Avancées :
 # Convertissez l'âge en float.
 # Multipliez la taille par l'âge et stockez le résultat dans une nouvelle variable appelée resultat.
 # Affichez le résultat.
+
+str(age)
+def multiplication(x: int, y: float) -> int:
+    resultat = x * y
+    return resultat
+
+resultat= multiplication(x=age, y=taille)
+print(resultat)
+
 # Conseils :
 # Utilisez la fonction input() pour obtenir des valeurs de l'utilisateur.
 # Pensez à convertir les entrées de l'utilisateur en types appropriés (par exemple, utilisez int() pour convertir une entrée en entier).
